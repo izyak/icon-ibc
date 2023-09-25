0d3b96963541c907f2c6bc9a83e4924702be0e46
Deepak Bomjan <dpaktamang@gmail.com>
2023-09-25 09:11:58 +0545

    add deployment machine setup scripts

---
+++ deployer/bin/check-parameter.sh
@@ -0,0 +1,10 @@
+#!/bin/bash
+echo
+echo "#######  Check Loaded Env Parameters ########"
+echo
+echo
+cd /opt/deployer/root/icon-ibc
+source ./const.sh
+for var in $(cat const.sh | grep ^export | grep -v 'PASSWORD' | awk '{print $2}' | awk -F\= '{print $1}');do
+	env | grep "$var"
+done
