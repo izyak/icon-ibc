d44dc8c1cbf1bd1a1e75707d6943c23490efa521
Deepak Bomjan <dpaktamang@gmail.com>
2023-09-27 16:29:12 +0545

    typo

--- deployer/bin/fetch_keys.sh
+++ deployer/bin/fetch_keys.sh
@@ -10,7 +10,7 @@ python3 /opt/deployer/root/keyutils/decrypt.py >/dev/null 2>&1
 echo "Checking ICON ibc wallet ..."
 goloop ks verify ${KEYSTORE}/ibc_wallet.json -p $(grep 'icon_ibc_wallet_secret' $PASSWORD_FILE | awk -F\" '{print $4}') 2>/dev/null || echo FAILED
 echo "Checking ICON xcall wallet ..."
-goloop ks verify ${KEYSTORE}/ibc_wallet.json -p $(grep 'icon_xcall_wallet_secret' $PASSWORD_FILE | awk -F\" '{print $4}') 2>/dev/null || echo FAILED
+goloop ks verify ${KEYSTORE}/xcall_wallet.json -p $(grep 'icon_xcall_wallet_secret' $PASSWORD_FILE | awk -F\" '{print $4}') 2>/dev/null || echo FAILED
 echo "Checking Archway ibc wallet ..."
 archwayd keys list --keyring-backend test | grep -q ibc_wallet && echo SUCCESS || echo FAILED
 echo "Checking Archway xcall wallet ..."
