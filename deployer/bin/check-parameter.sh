469b89dd4534b7c9144bd2c35271a68017627b17
izyak <lbackup887@gmail.com>
2023-09-28 15:43:33 +0545

    fix: remove sudo

--- deployer/bin/check-parameter.sh
+++ deployer/bin/check-parameter.sh
@@ -4,7 +4,7 @@ echo "#######  Check Loaded Env Parameters ########"
 echo
 echo
 cd /opt/deployer/root/icon-ibc
-sudo source ./const.sh
+source ./const.sh
 for var in $(cat const.sh | grep ^export | grep -v 'PASSWORD' | awk '{print $2}' | awk -F\= '{print $1}');do
 	env | grep "$var"
 done
