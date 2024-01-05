a912434dea44a3451540b4a618c6f21aff613bed
Deepak Bomjan <dpaktamang@gmail.com>
2024-01-05 08:14:26 +0545

    correct filename

--- 3_upgrade.sh
+++ 3_upgrade.sh
@@ -11,19 +11,24 @@ source const.sh
 echo "Fetching contracts"
 WORK_DIR=$PWD
 
-ICON_TM_CLIENT=$WORK_DIR/artifacts/xcall-connection-0.1.2-optimized.jar
 
+
+
+
+# Fetch contracts to update
+
+echo
+
+
+ICON_XCALL_CONNECTION=$WORK_DIR/artifacts/xcall-connection-0.1.2-optimized.jar
 
 # Navigate to working directory
 cd $WORK_DIR
 
-# Fetch contracts to update
-curl -L https://github.com/icon-project/IBC-Integration/releases/download/v1.1.2/xcall-connection-0.1.2-optimized.jar --output $ICON_TM_CLIENT
 
-echo
-
-# update xcall on icon
-if [ ! -f $LOGS/"$ICON_CHAIN_ID"_"1_tm_client_upgrade" ]; then
-	update_contract light-client $ICON_TM_CLIENT ibcHandler=cx622bbab73698f37dbef53955fd3decffeb0b0c16 update=0x1
-	echo 1 > $LOGS/"$ICON_CHAIN_ID"_"1_tm_client_upgrade"
+curl -L https://github.com/icon-project/IBC-Integration/releases/download/v1.1.2/xcall-connection-0.1.2-optimized.jar --output $ICON_XCALL_CONNECTION
+echo 
+if [ ! -f $LOGS/"$ICON_CHAIN_ID"_"1.2_xcall_connection_upgrade" ]; then
+    update_contract xcall-connection $ICON_XCALL_CONNECTION _xCall=cxa07f426062a1384bdd762afa6a87d123fbc81c75  _ibc=cx622bbab73698f37dbef53955fd3decffeb0b0c16  _port=xcall
+    echo 1 > $LOGS/"$ICON_CHAIN_ID"_"1.2_xcall_connection_upgrade"
 fi
