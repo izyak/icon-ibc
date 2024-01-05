0b18b9a9f9b4126bf4131191d9eaec6ae2ca1b68
izyak <lbackup887@gmail.com>
2024-01-05 15:21:35 +0545

    fix

--- 4_upgrade.sh
+++ 4_upgrade.sh
@@ -20,15 +20,15 @@ fi
 echo "Proceeding with instantiating new xcall..."
 # rm $CONTRACT_ADDR_WASM_XCALL
 rm /opt/deployer/root/icon-ibc/ixc/injective-1_set_xcall_host_1
-rm /opt/deployer/root/icon-ibc/ixc/injective-1_set_default_connection
+rm /opt/deployer/root/icon-ibc/ixc/injective-1_set_xcall_host
+# rm /opt/deployer/root/icon-ibc/ixc/injective-1_set_default_connection
 
 # instantiate new xcall
-if [ ! -f $LOGS/"$WASM_CHAIN_ID"_"xcall_instantiate_2" ]; then
-	xcall_args="{\"network_id\":\"injective-1\",\"denom\":\"inj\"}"
-	deploy_contract $CONTRACT_FILE_WASM_XCALL $CONTRACT_ADDR_WASM_XCALL ${xcall_args} "$WASM_XCALL_COMMON_ARGS" $WASM_XCALL_WALLET "${INJ_CODE_ID[cw_xcall]}"
-	echo 1 > $LOGS/"$WASM_CHAIN_ID"_"xcall_instantiate_2"
-fi
-
+# if [ ! -f $LOGS/"$WASM_CHAIN_ID"_"xcall_instantiate_2" ]; then
+# 	xcall_args="{\"network_id\":\"injective-1\",\"denom\":\"inj\"}"
+# 	deploy_contract $CONTRACT_FILE_WASM_XCALL $CONTRACT_ADDR_WASM_XCALL ${xcall_args} "$WASM_XCALL_COMMON_ARGS" $WASM_XCALL_WALLET "${INJ_CODE_ID[cw_xcall]}"
+# 	echo 1 > $LOGS/"$WASM_CHAIN_ID"_"xcall_instantiate_2"
+# fi
 
 # set xcall host on xcall-connection
 if [ ! -f $LOGS/"$WASM_CHAIN_ID"_"set_xcall_host_1" ]; then
@@ -37,11 +37,11 @@ if [ ! -f $LOGS/"$WASM_CHAIN_ID"_"set_xcall_host_1" ]; then
 fi
 
 # delete prev log that prevents setting default connection
-rm $LOGS/"$WASM_CHAIN_ID"_"set_default_connection"
+# rm $LOGS/"$WASM_CHAIN_ID"_"set_default_connection"
 
-# set default connection for icon
-if [ ! -f $LOGS/"$WASM_CHAIN_ID"_"set_default_connection_1" ]; then
-	set_default_connection
-	echo 1 > $LOGS/"$WASM_CHAIN_ID"_"set_default_connection_1"
-fi
+# # set default connection for icon
+# if [ ! -f $LOGS/"$WASM_CHAIN_ID"_"set_default_connection_1" ]; then
+# 	set_default_connection
+# 	echo 1 > $LOGS/"$WASM_CHAIN_ID"_"set_default_connection_1"
+# fi
 
